9837f79c188be620499c7060a9bf377446a15675
huskyhus <hushushus@outlook.jp>
2025-09-27 17:21:54 +0000

    chore(learning_go): add comment in chap2

--- pkg/learning_go/chap2.go
+++ pkg/learning_go/chap2.go
@@ -7,27 +7,38 @@ import (
 
 func BasicTypes() {
 	fmt.Println("Basic Types: begin")
+
+	// bool
 	var b = true
 	fmt.Println(b && false)
 	fmt.Println(b || false)
 	fmt.Println(!b)
 
+	// int
 	var i = 12345
 	fmt.Println(i + 100)
 	fmt.Println(i - 100)
 	fmt.Println(i * 100)
 	fmt.Println(i / 100)
 
+	// float64
 	var f = 3.1415926535
 	fmt.Println(math.Pow(f, 100))
 	fmt.Println(math.Pow(f, 0.01))
 
+	// rune = int32
 	var r = 'A'
 	fmt.Println(r)
 
+	// string
 	var s = "Hello, World!"
 	fmt.Println(s)
 
+	// byte = uint8
+	var char = s[3]
+	fmt.Println(char)
+
+	// some variants to declare variables
 	var (
 		listed       = 10
 		values       = "string"
@@ -40,6 +51,7 @@ func BasicTypes() {
 	shorthand := "shorthand"
 	fmt.Println(shorthand)
 
+	// constant
 	const constant int = 42
 	// constant += 100
 	fmt.Println(constant)
